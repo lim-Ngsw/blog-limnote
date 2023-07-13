7294af4a8dda3d104230f595e303618aaa8f1775
lim-Ngsw <ancelotty@gmail.com>
2023-07-13 22:54:34 +0900

    doc:準備

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+}
